de4da4431ec38853687d66d74587f8888a16cdf3
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-21 14:49:06 -0800

    Update Total From Text File.py

--- Total From Text File.py
+++ Total From Text File.py
@@ -4,7 +4,6 @@ with open("Yearly Cost.txt") as file:
     for line in file:
         line_list = line.split("$")
         line_amount = line_list[1].replace('\n', '')
-        line_amount = float(line_amount)
-        total = float(total) + line_amount
+        total = float(total) + float(line_amount)
 print("Yearly Total: $" + str(total))
 file.close()
