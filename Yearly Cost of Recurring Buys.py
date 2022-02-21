166ad222fa2f6ac9b8e7afb16d1a0dd909e0dd6c
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-20 21:56:07 -0800

    Create Yearly Cost of Recurring Buys.py

---
+++ Yearly Cost of Recurring Buys.py
@@ -0,0 +1,16 @@
+item_name = input("Item name: ")
+item_cost = input("Item cost: ")
+item_restock_time = input("Restock every (number + days/weeks/months): ")
+item_restock_time_list = item_restock_time.split(" ")
+number_restock = item_restock_time_list[0]
+if item_restock_time.find("day") != -1:
+    yearly_cost = float(item_cost) / float(number_restock) * 365
+if item_restock_time.find("week") != -1:
+    yearly_cost = float(item_cost) / float(number_restock) * 52
+if item_restock_time.find("month") != -1:
+    yearly_cost = float(item_cost) / float(number_restock) * 12
+yearly_cost = format(yearly_cost, '.2f')
+print(item_name + " : $" + str(yearly_cost) + " every year\n")
+with open("Yearly Cost.txt", "a") as save:
+    save.write(item_name + " : $" + str(yearly_cost) + "\n")
+save.close()
