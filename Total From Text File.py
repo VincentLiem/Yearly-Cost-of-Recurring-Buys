994d38bea78a5e2e66ecb9724af5f539e21c6735
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-21 14:56:34 -0800

    Update Total From Text File.py

--- Total From Text File.py
+++ Total From Text File.py
@@ -1,9 +1,18 @@
-total = 0.00
-total = format(total, ".2f")
+year_total = 0.00
 with open("Yearly Cost.txt") as file:
     for line in file:
         line_list = line.split("$")
         line_amount = line_list[1].replace('\n', '')
-        total = float(total) + float(line_amount)
-print("Yearly Total: $" + str(total))
+        year_total = float(year_total) + float(line_amount)
+        year_total = format(year_total, ".2f")
+print("Yearly Total: $" + str(year_total))
+month_total = float(year_total) / 12
+month_total = format(month_total, ".2f")
+week_total = float(year_total) / 52
+week_total = format(week_total, ".2f")
+day_total = float(year_total) / 365
+day_total = format(day_total, ".2f")
+print("Monthly Total: $" + str(month_total))
+print("Yearly Total: $" + str(week_total))
+print("Daily Total: $" + str(day_total))
 file.close()
