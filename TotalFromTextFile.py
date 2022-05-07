4329ea4cd4e66f90928b2ebe966913f1fc4b452d
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-06 17:08:37 -0700

    Update and rename Total From Text File.py to TotalFromTextFile.py

---
+++ TotalFromTextFile.py
@@ -0,0 +1,12 @@
+from TotalFromCSVFile import *
+try:
+    year_total = 0.00
+    with open('Yearly Cost.txt') as file:
+        for line in file:
+            line_list = line.split('$')
+            line_amount = line_list[1].replace('\n', '')
+            year_total = float(year_total) + float(line_amount)
+            year_total = format(year_total, '.2f')
+    print_from_year_total(year_total)
+except FileNotFoundError:
+    print("Yearly Cost.txt not created yet")
